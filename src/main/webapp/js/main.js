190ee6bad21bc72496ddede9787e55578bf53507
Ivan <inavarro@testserver.(none)>
2014-07-18 16:46:28 +0200

    UI with initial searchs

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,15 +1,26 @@
-$(function() {
-$.getJSON('node/_cluster/nodes/_local', function(data) {
-console.log(data);
-/* 
-var node_id = Object.keys(data.nodes)[0]
-, cluster_name = data['cluster_name']
-, node_name = data.nodes[node_id]['name']
-, host_name = data.nodes[node_id]['hostname'];
- 
-$('#cluster_name').html( cluster_name);
-$('#node_name').html( node_name );
-$('#host_name').html( host_name );
-*/
-})
-});
+var result;
+function doSearch() {
+	
+	pst = $('#pst').val();
+	query = $('#query').val();
+	
+	url = '/kostal/rest/search';
+	if (pst != 'all') {
+		url = url + '/' + pst;
+	}
+	url = url + '?q=' + query;
+	
+	$.getJSON(url, function(data) {
+		result = data;
+		
+		xx = [];
+		console.log(Object.keys(result[0][0]['_source']));
+		for(var i=0; i<result.length; i++){
+			xx.push('<p>' + result[i][0]['_source']['conversationTopic'] + " || " + result[i][0]['_source']['clientSubmitTime'] + '</p>');
+		}
+			
+		$('#results').html(xx.toString());
+	});
+	
+	return true;
+};
